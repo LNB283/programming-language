c8e782f20b1e73c241ed99a20a6b9b7d59d69527
LNB283 <nebout.bayard@gmail.com>
2020-11-02 16:08:32 +0900

    commit

---
+++ GO-and-Web/Template/Different-types-into-Templates/Method/main.go
@@ -0,0 +1,47 @@
+/////////////////main.go/////////////////////////////////////////////////////////////////////////
+///objective(s):
+///				- Create 2 functions : square , sqaure root
+///				- In the template , use these functions
+/////////////////////////////////////////////////////////////////////////////////////////////////
+package main
+
+import (
+	"log"
+	"math"
+	"os"
+	"text/template"
+)
+
+type val struct {
+	Value int
+}
+
+func (v val) Double() int {
+	return v.Value * 2
+}
+
+func (v val) SquareRoot() float64 {
+	return math.Sqrt(float64(v.Value))
+}
+
+func (v val) Square() int {
+	return v.Value * v.Value
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("methodtemplate.gohtml"))
+}
+
+func main() {
+
+	v := val{
+		Value: 56,
+	}
+
+	err := tpl.Execute(os.Stdout, v)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
